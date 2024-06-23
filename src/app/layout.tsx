aae6c049e1a748f7d429fd5e5cdf231f41d2ea87
zahidhasanemran <zahid.emran@thealtcom.com>
2024-06-23 06:00:12 -0400

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="bg-orange-600 px-8 py-6">Header</header>
+        <main className="py-16 px-16">{children}</main>
+        <footer className="bg-sky-600 px-8 py-6">Footer</footer>
+      </body>
     </html>
   )
 }
