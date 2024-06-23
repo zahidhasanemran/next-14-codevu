270cc4f81eb35898b3fc97e93f98ad2ea7e287ce
zahidhasanemran <zahid.emran@thealtcom.com>
2024-06-23 00:00:01 -0400

    Catch all Segments

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CatchAllRoutes = ({ params }: { params: { slug: string[] } }) => {
+  return (
+    <div>
+      <h2>Catch All Routes </h2>
+      <ul>
+        {params.slug.map((sm, i) => (
+          <li key={i}>{sm}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default CatchAllRoutes
