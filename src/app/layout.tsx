c29c8a551d718f4ab00dd47a4287ff370a82e3c5
zahidhasanemran <zahid.emran@thealtcom.com>
2024-06-23 06:04:42 -0400

    default layout edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="bg-orange-600 px-8 py-6">Header</header>
+        <header className="bg-orange-600 px-8 py-6">
+          <ul className="flex items-center ">
+            <li className="px-4 py-3 text-white">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="px-4 py-3 text-white">
+              <Link href="/login">Login</Link>
+            </li>
+            <li className="px-4 py-3 text-white">
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li className="px-4 py-3 text-white">
+              <Link href="/docs">Docs</Link>
+            </li>
+          </ul>
+        </header>
         <main className="py-16 px-16">{children}</main>
-        <footer className="bg-sky-600 px-8 py-6">Footer</footer>
+        <footer className="bg-sky-600 px-8 py-6 text-white text-2xl ">
+          Footer
+        </footer>
       </body>
     </html>
   )
